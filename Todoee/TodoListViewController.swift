6bf33e1a45e714cc543ee55b5e9e637c01316916
Kashif Ali <kashifali874@gmail.com>
2018-11-01 23:46:47 -0700

    Basic functionality of the todo is added, checkmark and UITableView delegate methods.

--- Todoee/TodoListViewController.swift
+++ Todoee/TodoListViewController.swift
@@ -17,19 +17,38 @@ class TodoListViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-//  Number of cells to be populated on the screen
+    // MARK: numberOfRowsInSection
+    //    Number of cells to be populated on the screen
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-    
+    //Mark: cellForRowAT
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        Indicating towards the cell via identifier
+        //        Indicating towards the cell via identifier
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        
+        //        Setting the text of the table to use it from hard coded array
         cell.textLabel?.text = itemArray[indexPath.row]
         
         return cell
     }
+
+    //    Mark: Table View Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        
+//        Checkes tick mark if cell is selected
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+//        Deselectes the rows after the tap
+        tableView.deselectRow(at: indexPath, animated: true)
+
+    }
+    
 
 
 }
